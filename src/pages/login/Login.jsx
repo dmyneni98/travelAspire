e13460b1ec9b4a680bd18786483d3dcd1c061617
PeilingCai8059 <99828285+PeilingCai8059@users.noreply.github.com>
2022-11-16 17:10:50 -0600

    Add files via upload

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,19 @@
+
+
+import React from "react"
+import Header from "../../components/header/Header"
+import Footer from "../../components/footer/Footer"
+import LoginInfo from "../../components/login/LoginInfo"
+
+function Login(){
+    return(
+        <div >
+            <Header/>
+            <LoginInfo/>
+            <Footer/>
+        </div>
+        
+    )
+}
+
+export default Login
